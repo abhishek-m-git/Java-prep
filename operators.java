c5b2fba9b1ecdc3da76665aaa0e5b8c9aa7ae4b5
Abhishek Mali <abhishekmali4402@gmail.com>
2025-06-30 02:30:06 +0530

    operators

--- operators.java
+++ operators.java
@@ -5,9 +5,20 @@ public class operators {
         int num1 = 10;
         int num2 = 20;
         
-        int result = num1 +num2;
+        // int result = num1 +num2;
 
-        System.out.println(result);
+        // System.out.println(result);
+
+
+        // num1 = num1 +20;
+        //num1-=20;
+        // num1--;
+
+        int result1 = num1++;    //first fetch the value and then increment
+        int result2 = ++num1;    //first increment the value and then fetch
+        
+
+        System.out.println(result1);
 
 
     }
